054ec70afda1594b567f52030d62278e923d5190
Iuliia <ytaratynova@gmail.com>
2024-04-30 17:21:05 +0300

    + blog page with vue

---
+++ final/blog.js
@@ -0,0 +1,41 @@
+new Vue({
+    el: "#blog",
+    data: {
+        blogItems: [{
+            img: './images/blog_1.jpg',
+            title: "Let’s Get Solution For Building Construction Work",
+            date: "22 December, 2022",
+            tag: "Kitchan Design"
+        },
+        {
+            img: './images/blog_2.jpg',
+            title: "Low Cost Latest Invented Interior Designing Ideas.",
+            date: "25 December, 2022",
+            tag: "Living Design"
+        },
+        {
+            img: './images/blog_3.jpg',
+            title: "Best For Any Office & Business Interior Solution",
+            date: "26 December, 2022",
+            tag: "Interior Design"
+        },
+        {
+            img: './images/blog_4.jpg',
+            title: "Low Cost Latest Invented Interior Designing Ideas.",
+            date: "22 December, 2022",
+            tag: "Kitchan Design"
+        },
+        {
+            img: './images/blog_5.jpg',
+            title: "Low Cost Latest Invented Interior Designing Ideas.",
+            date: "25 December, 2022",
+            tag: "Living Design"
+        },
+        {
+            img: './images/blog_6.jpg',
+            title: "Best For Any Office & Business Interior Solution",
+            date: "26 December, 2022",
+            tag: "Interior Design"
+        }]
+    }
+})
